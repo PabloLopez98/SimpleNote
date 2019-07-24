7913d11bd45aef672662fed79039ad4e2ec1a02e
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2019-07-24 15:12:59 -0700

    up next: implement permission request

--- app/src/main/java/pablo/myexample/simplenote/MainActivity.java
+++ app/src/main/java/pablo/myexample/simplenote/MainActivity.java
@@ -2,26 +2,36 @@ package pablo.myexample.simplenote;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
     public static final int EDIT_WORD_ACTIVITY_REQUEST_CODE = 2;
+    public static final int REQUEST_MICROPHONE = 1;
 
     private NoteViewModel mNoteViewModel;
 
@@ -48,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkMicPermssion();
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         final NoteListAdapter adapter = new NoteListAdapter(this);
@@ -90,6 +102,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private void checkMicPermssion() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    REQUEST_MICROPHONE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_MICROPHONE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted to record audio", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Permissions Denied to record audio", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+
 
     public void toTypeNote(View view) {
         Intent intent = new Intent(MainActivity.this, TypeNote.class);
--- app/src/main/java/pablo/myexample/simplenote/TypeNote.java
+++ app/src/main/java/pablo/myexample/simplenote/TypeNote.java
@@ -1,42 +1,57 @@
 package pablo.myexample.simplenote;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class TypeNote extends AppCompatActivity {
-    public static final String EXTRA_ID =
-            "THE_EXTRA_ID";
-    public static final String EXTRA_TITLE =
-            "THE_EXTRA_TITLE";
-    public static final String EXTRA_DESCRIPTION =
-            "THE_EXTRA_DESCRIPTION";
-    public static final String EXTRA_NOTE_TITLE =
-            "THE_EXTRA_NOTE_TITLE";
+    public static final String EXTRA_ID = "THE_EXTRA_ID";
+    public static final String EXTRA_TITLE = "THE_EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION = "THE_EXTRA_DESCRIPTION";
+    public static final String EXTRA_NOTE_TITLE = "THE_EXTRA_NOTE_TITLE";
 
     private EditText inputTitle;
     private EditText inputDescription;
     private String sameTitle;
+    private Intent replyIntent;
+    private Intent intent;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type_note);
-
+        title = findViewById(R.id.toolbartwo);
+        replyIntent = new Intent();
         inputTitle = findViewById(R.id.inputTitle);
         inputDescription = findViewById(R.id.inputDescription);
 
         final Button saveButton = findViewById(R.id.savebutton);
 
-        final Intent intent = getIntent();
+        intent = getIntent();
 
         if (intent.hasExtra(EXTRA_ID)) {
+            title.setText("Update Note");
             sameTitle = intent.getStringExtra(EXTRA_TITLE);
             inputTitle.setText(intent.getStringExtra(EXTRA_NOTE_TITLE));
             inputDescription.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
@@ -44,25 +59,119 @@ public class TypeNote extends AppCompatActivity {
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent replyIntent = new Intent();
-                if (TextUtils.isEmpty(inputTitle.getText())) {
-                    setResult(RESULT_CANCELED, replyIntent);
-                } else {
+                //replyIntent = new Intent();
+                if (TextUtils.isEmpty(inputTitle.getText()) || TextUtils.isEmpty(inputDescription.getText())) { //uh-oh there is a missing input field
+                    Toast.makeText(getApplicationContext(), "Missing input", Toast.LENGTH_LONG).show();
+                } else { //good to go and get saved
                     String word = inputTitle.getText().toString();
                     String description = inputDescription.getText().toString();
 
-                    if(intent.hasExtra(EXTRA_ID)){
-                        replyIntent.putExtra(EXTRA_TITLE,sameTitle);
-                    }else {
+                    if (intent.hasExtra(EXTRA_ID)) {
+                        replyIntent.putExtra(EXTRA_TITLE, sameTitle);
+                    } else {
                         replyIntent.putExtra(EXTRA_TITLE, word);
                     }
                     replyIntent.putExtra(EXTRA_NOTE_TITLE, word);
                     replyIntent.putExtra(EXTRA_DESCRIPTION, description);
                     setResult(RESULT_OK, replyIntent);
+                    finish();
                 }
+            }
+        });
+
+        final SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        final Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+            }
+
+            @Override
+            public void onError(int i) {
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                //getting all the matches
+                ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                //displaying the first match
+                if (matches != null) inputDescription.setText(matches.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+            }
+        });
+
+        findViewById(R.id.micbutton).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        mSpeechRecognizer.stopListening();
+                        inputDescription.setHint("Description");
+                        break;
+                    case MotionEvent.ACTION_DOWN:
+                        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                        inputDescription.setText("");
+                        inputDescription.setHint("...Recording...");
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        alertInfo();
+    }
+
+    private void alertInfo() {
+        AlertDialog alertDialog = new AlertDialog.Builder(TypeNote.this).create();
+        alertDialog.setTitle("Careful!");
+        if(intent.hasExtra(EXTRA_ID)) {
+            alertDialog.setMessage("Discard changes?");
+        }else{
+            alertDialog.setMessage("Do you want to delete note?");
+        }
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                setResult(RESULT_CANCELED,replyIntent);
                 finish();
             }
         });
+        alertDialog.show();
     }
 
 
